a6513bda19e8cd275230b0584d6af13602a6d0d0
B Hold <Hotpockets01@users.noreply.github.com>
2015-05-15 12:03:41 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+//This is a game by VexityGames created by Brendon Ho on JavaScript
+//Peter Soboyejo is also a contributor
+var creator = "Brendon Ho";
+
+
+var keys = []
+var keyisPressed = function(){};
+var keyisReleased = function(){};
+
+//player functions
+var graviy: 0.4;
+var fallSpeed = 8;
